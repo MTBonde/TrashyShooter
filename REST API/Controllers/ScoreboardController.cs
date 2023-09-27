ca24391aed19953abaaeef98f174004203dede1b
NillerA <113096753+NillerA@users.noreply.github.com>
2023-09-27 09:40:10 +0200

    rest api

---
+++ REST API/Controllers/ScoreboardController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace REST_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ScoreboardController : ControllerBase
+    {
+        private readonly Dictionary<int, ScoreboardModel> _scoreboard;
+
+        public ScoreboardController()
+        {
+            _scoreboard = new Dictionary<int, ScoreboardModel>();
+        }
+
+
+        [HttpGet]
+        public ActionResult<IEnumerable<ScoreboardModel>> Get()
+        {
+            return Ok(_scoreboard.Values);
+
+        }
+
+
+        [HttpPost]
+        public IActionResult Post([FromBody] ScoreboardModel newEntry)
+        {
+            if (newEntry == null)
+            {
+                return BadRequest("Invalid Data");
+            }
+
+            int newId = _scoreboard.Count + 1;
+            newEntry.ID = newId;
+
+            _scoreboard[newId] = newEntry;
+
+            return CreatedAtAction(nameof(Get), new { id = newEntry.ID }, newEntry);
+
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] ScoreboardModel updateClient)
+        {
+            if (!_scoreboard.ContainsKey(id)) 
+            { 
+                return NotFound();
+            }
+
+            updateClient.ID = id;
+            _scoreboard[id] = updateClient;
+            return NoContent();
+        }
+
+        
+    }
+}
---
+++ REST API/ScoreboardModel.cs
@@ -0,0 +1,9 @@
+﻿namespace REST_API
+{
+    public class ScoreboardModel
+    {
+        public int ID { get; set; }
+        public string playerName { get; set; }
+        public int score { get; set; }
+    }
+}
