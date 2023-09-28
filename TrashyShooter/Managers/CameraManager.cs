c88b35c09aee219309c5818a1b78ca0ccd7fcbfc
Niels Andersen <113096753+NillerA@users.noreply.github.com>
2023-09-28 21:53:51 +0200

    models

--- TrashyShooter/Managers/CameraManager.cs
+++ TrashyShooter/Managers/CameraManager.cs
@@ -31,8 +31,8 @@ namespace MultiplayerEngine
 
             effect.FogEnabled = !Globals.DebugModeToggled;
             effect.FogColor = Color.Black.ToVector3();
-            effect.FogStart = 0f;
-            effect.FogEnd = 3f;
+            effect.FogStart = 3f;
+            effect.FogEnd = 10f;
         }
 
         public static void ApplyCameraEffects(SpriteBatch spriteBatch)
--- TrashyShooter/Scenes/TestScene.cs
+++ TrashyShooter/Scenes/TestScene.cs
@@ -19,10 +19,20 @@ namespace MultiplayerEngine
             //creates worlds center point
             worldMatrix = Matrix.CreateWorld(Vector3.Zero, Vector3.Forward, Vector3.Up);
 
-            GameObject floor = new GameObject();
-            floor.AddComponent<MeshRenderer>().SetModel("floor");
-            floor.transform.Position3D = new Vector3(0, 0, 0);
-            floor.transform.Rotation = new Vector3(90, 0, 0);
+            int floorCount = 5;
+            int floorScale = 5;
+            for (int x = 0; x < floorCount; x++)
+            {
+                for (int y = 0; y < floorCount; y++)
+                {
+                    GameObject floor = new GameObject();
+                    floor.AddComponent<MeshRenderer>().SetModel("floor");
+                    floor.GetComponent<MeshRenderer>().scale = 1.0f;
+                    Debug.WriteLine(y * floorCount - (floorCount * floorScale / 2));
+                    floor.transform.Position3D = new Vector3(x * floorScale - (floorCount * floorScale / 2), y * floorCount - (floorCount * floorScale / 2), 0);
+                    floor.transform.Rotation = new Vector3(90, 0, 0);
+                }
+            }
 
             //GameObject roof = new GameObject();
             //roof.AddComponent<MeshRenderer>().SetModel("floor");
@@ -32,7 +42,7 @@ namespace MultiplayerEngine
             foreach (Vector3 cubePos in WorldLayout.cubes)
             {
                 GameObject cube = new GameObject();
-                cube.AddComponent<MeshRenderer>().SetModel("Cube");
+                cube.AddComponent<MeshRenderer>().SetModel("Box");
                 cube.transform.Position3D = cubePos;
                 cube.transform.Rotation = new Vector3(0, 0, 0);
             }
