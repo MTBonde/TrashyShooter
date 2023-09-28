6979e45212a7073926e1e42857af79a059f69e9f
Niels Andersen <113096753+NillerA@users.noreply.github.com>
2023-09-28 22:30:13 +0200

    bane

--- TrashyShooter/Scenes/TestScene.cs
+++ TrashyShooter/Scenes/TestScene.cs
@@ -19,19 +19,43 @@ namespace MultiplayerEngine
             //creates worlds center point
             worldMatrix = Matrix.CreateWorld(Vector3.Zero, Vector3.Forward, Vector3.Up);
 
-            int floorCount = 5;
+            int floorCount = 6;
             int floorScale = 5;
+            int size = floorCount * floorScale;
+            float offsetFix = floorScale / 2 + 0.5f;
             for (int x = 0; x < floorCount; x++)
             {
                 for (int y = 0; y < floorCount; y++)
                 {
                     GameObject floor = new GameObject();
                     floor.AddComponent<MeshRenderer>().SetModel("floor");
-                    floor.GetComponent<MeshRenderer>().scale = 1.0f;
-                    Debug.WriteLine(y * floorCount - (floorCount * floorScale / 2));
-                    floor.transform.Position3D = new Vector3(x * floorScale - (floorCount * floorScale / 2), y * floorCount - (floorCount * floorScale / 2), 0);
+                    floor.transform.Position3D = new Vector3(x * floorScale - (size / 2) + offsetFix, y * floorScale - (size / 2) + offsetFix, 0);
                     floor.transform.Rotation = new Vector3(90, 0, 0);
                 }
+            }
+
+            for (int x = 0;x < size / 2; x++)
+            {
+                GameObject wall = new GameObject();
+                wall.AddComponent<MeshRenderer>().SetModel("ScifiWall");
+                wall.transform.Position3D = new Vector3(x * 2 - (size / 2) + 1, size / 2, 0);
+                wall.transform.Rotation = new Vector3(90, 0, 270);
+                wall = new GameObject();
+                wall.AddComponent<MeshRenderer>().SetModel("ScifiWall");
+                wall.transform.Position3D = new Vector3(x * 2 - (size / 2) + 1, -(size / 2), 0);
+                wall.transform.Rotation = new Vector3(90, 0, 90);
+            }
+
+            for (int y = 0; y < floorCount * floorScale / 2; y++)
+            {
+                GameObject wall = new GameObject();
+                wall.AddComponent<MeshRenderer>().SetModel("ScifiWall");
+                wall.transform.Position3D = new Vector3(size / 2, y * 2 - (size / 2) + 1, 0);
+                wall.transform.Rotation = new Vector3(90, 0, 180);
+                wall = new GameObject();
+                wall.AddComponent<MeshRenderer>().SetModel("ScifiWall");
+                wall.transform.Position3D = new Vector3(-(size / 2), y * 2 - (size / 2) + 1, 0);
+                wall.transform.Rotation = new Vector3(90, 0, 0);
             }
 
             //GameObject roof = new GameObject();
