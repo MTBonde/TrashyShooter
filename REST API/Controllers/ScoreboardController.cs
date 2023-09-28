c05fba3078691428588104963f3d48fe9b2b8cc2
MTBonde <Thorbonde@gmail.com>
2023-09-28 20:53:19 +0200

    Rest kommenteret

--- REST API/Controllers/ScoreboardController.cs
+++ REST API/Controllers/ScoreboardController.cs
@@ -4,25 +4,38 @@ using System.Linq;
 
 namespace REST_API.Controllers
 {
+    /// <summary>
+    /// Kontrollerer API-Endpoint for scoretavlen. Håndterer CRUD-operationer for spilscore.
+    /// </summary>
     [ApiController]
     [Route("api/[controller]")]
     public class ScoreboardController : ControllerBase
     {
         private static  Dictionary<int, ScoreboardModel> _scoreboard = new Dictionary<int, ScoreboardModel>();
 
+        /// <summary>
+        /// Initialiserer en ny instans af ScoreboardController klassen.
+        /// </summary>
         public ScoreboardController()
         {
 
         }
 
-
+        /// <summary>
+        /// Henter alle scoreposter fra scoretavlen.
+        /// </summary>
+        /// <returns>En liste af ScoreboardModel objekter.</returns>
         [HttpGet("GetScoreboard")]
         public ActionResult<IEnumerable<ScoreboardModel>> Get()
         {
             return Ok(_scoreboard.Values);
         }
 
-
+        /// <summary>
+        /// Tilføjer en ny scorepost til scoretavlen.
+        /// </summary>
+        /// <param name="newEntry">Det nye ScoreboardModel objekt, der skal tilføjes.</param>
+        /// <returns>ActionResult repræsentation af operationens udfald.</returns>
         [HttpPost("PostScore")]
         public IActionResult Post([FromBody] ScoreboardModel newEntry)
         {
@@ -40,6 +53,12 @@ namespace REST_API.Controllers
 
         }
 
+        /// <summary>
+        /// Opdaterer en eksisterende scorepost i scoretavlen.
+        /// </summary>
+        /// <param name="id">ID'et for den scorepost, der skal opdateres.</param>
+        /// <param name="updateClient">Det opdaterede ScoreboardModel objekt.</param>
+        /// <returns>ActionResult repræsentation af operationens udfald.</returns>
         [HttpPut("{id}")]
         public IActionResult Put(int id, [FromBody] ScoreboardModel updateClient)
         {
